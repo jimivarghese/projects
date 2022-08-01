5419eaa027ed54cc94e9e86504a5512be1077d09
jimivarghese <109644751+jimivarghese@users.noreply.github.com>
2022-08-01 16:49:14 +0530

    updating

--- springbootProject/calculator-react/react-calculator/src/components/Calculator.js
+++ springbootProject/calculator-react/react-calculator/src/components/Calculator.js
@@ -14,7 +14,7 @@ import "./Calculator.css";
         const calculate=()=>
         {
             try{
-              let answer =eval(inputValue);
+              let answer =String(eval(inputValue));
              setInputValue(answer);
 
             }
@@ -30,8 +30,18 @@ import "./Calculator.css";
         };
         const backspace=()=>
         {
+          var k=inputValue
+          if(inputValue==="Infinity"|| inputValue==="Error"||inputValue==="undefined")
+          
+            {k="";}
+          
+            else{k=inputValue.slice(0,-1); }
+            
+             setInputValue(k)
+
+          
          
-            setInputValue(inputValue.slice(0,-1));
+            //setInputValue(inputValue.slice(0,-1));
 
           
           
@@ -40,39 +50,39 @@ import "./Calculator.css";
   return (
    
 
-<div class="divone col-md-3">
+<div className="divone col-md-3">
    
         <input type="text" id="screen" placeholder="0" value={inputValue}/>
         <br/>
         
-        <button class="button number" onClick={()=> {display("9")}}>9</button>
-        <button class="button number" onClick={()=> {display("8")}}>8</button>
-        <button class="button number" onClick={()=> {display("7")}}>7</button>
+        <button className="button number" onClick={()=> {display("9")}}>9</button>
+        <button className="button number" onClick={()=> {display("8")}}>8</button>
+        <button className="button number" onClick={()=> {display("7")}}>7</button>
         <br/>
-        <button class="button number" onClick={()=> {display("6")}}>6</button>
-        <button class="button number" onClick={()=> {display("5")}}>5</button>
-        <button class="button number" onClick={()=> {display("4")}}>4</button>
+        <button className="button number" onClick={()=> {display("6")}}>6</button>
+        <button className="button number" onClick={()=> {display("5")}}>5</button>
+        <button className="button number" onClick={()=> {display("4")}}>4</button>
         <br/>
-        <button class="button number" onClick={()=> {display("3")}}>3</button>
-        <button class="button number" onClick={()=> {display("2")}}>2</button>
-        <button class="button number" onClick={()=> {display("1")}}>1</button>
+        <button className="button number" onClick={()=> {display("3")}}>3</button>
+        <button className="button number" onClick={()=> {display("2")}}>2</button>
+        <button className="button number" onClick={()=> {display("1")}}>1</button>
         <br/>
-        <button class="button operator" onClick={()=> {display(".")}}>.</button>
-        <button class="button number" onClick={()=> {display("0")}}>0</button>
-        <button class="button operator" onClick={()=> {display("/")}}>/</button>
+        <button className="button operator" onClick={()=> {display(".")}}>.</button>
+        <button className="button number" onClick={()=> {display("0")}}>0</button>
+        <button className="button operator" onClick={()=> {display("/")}}>/</button>
         <br/>
-        <button class="button operator" onClick={()=> {display("*")}}>*</button>
-        <button class="button operator" onClick={()=> {display("+")}}>+</button>
+        <button className="button operator" onClick={()=> {display("*")}}>*</button>
+        <button className="button operator" onClick={()=> {display("+")}}>+</button>
         
-        <button class="button operator" onClick={()=> {display("-")}}>-</button>
+        <button className="button operator" onClick={()=> {display("-")}}>-</button>
        
       
 
 <br/>
-<button class="button operator" onClick={()=> {clear();}}>C</button>
-<button class="button operator" onClick={()=> {backspace();}}>D</button>
+<button className="button operator" onClick={()=> {clear();}}>C</button>
+<button className="button operator" onClick={()=> {backspace();}}>D</button>
         
-        <button class="button result" onClick={()=> {calculate();}}>=</button>
+        <button className="button result" onClick={()=> {calculate();}}>=</button>
         
         
         
